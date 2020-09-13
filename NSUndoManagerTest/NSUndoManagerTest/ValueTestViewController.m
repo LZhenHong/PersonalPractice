a7f654415c2e46045204a554a1522f439bc1f1b8
LZhenHong <levine5blue@gmail.com>
2020-09-14 00:38:19 +0800

    [UPDATE] Support NSUndoManager redo.

--- NSUndoManagerTest/NSUndoManagerTest/ValueTestViewController.m
+++ NSUndoManagerTest/NSUndoManagerTest/ValueTestViewController.m
@@ -55,22 +55,41 @@
     return _testUndoMgr;
 }
 
-- (void)registerTestUndoHandle:(NSInteger)value {
-    [self.testUndoMgr registerUndoWithTarget:self selector:@selector(changeDisplayValue:) object:@(value)];
-    
-    [self _updateBarItemButtonStatus];
-}
-
 - (void)_updateBarItemButtonStatus {
     self.redoBarButtonItem.enabled = self.testUndoMgr.canRedo;
     self.undoBarButtonItem.enabled = self.testUndoMgr.canUndo;
 }
 
 - (void)changeDisplayValue:(NSNumber *)value {
-    [self registerTestUndoHandle:self.currentVal];
+    // 这个方法默认会把 selector 加到 Undo Stack，但是如果这个时候正在 Undoing，这个方法就会把 selector 加到 Redo Stack 中
+    // 有新的 selector 加到 Undo Stack 中，Redo Stack 会被清空
+    // Stack 是链表结构
+    [self.testUndoMgr registerUndoWithTarget:self selector:@selector(changeDisplayValue:) object:@(self.currentVal)];
+//    [self testUndoManagerStackLog];
     
     [self.displayValueLabel setText:[NSString stringWithFormat:@"%zd", value.integerValue]];
     self.currentVal = value.integerValue;
+    
+    [self _updateBarItemButtonStatus];
+}
+
+- (void)testUndoManagerStackLog {
+    if (self.testUndoMgr.isUndoing) {
+        id redoStack = [self.testUndoMgr valueForKey:@"_redoStack"];
+        NSLog(@"undoing redo stack: %@", [redoStack valueForKey:@"_count"]);
+        id undoStack = [self.testUndoMgr valueForKey:@"_undoStack"];
+        NSLog(@"undoing undo stack: %@", [undoStack valueForKey:@"_count"]);
+    } else if (self.testUndoMgr.isRedoing) {
+        id redoStack = [self.testUndoMgr valueForKey:@"_redoStack"];
+        NSLog(@"redoing redo stack: %@", [redoStack valueForKey:@"_count"]);
+        id undoStack = [self.testUndoMgr valueForKey:@"_undoStack"];
+        NSLog(@"redoing undo stack: %@", [undoStack valueForKey:@"_count"]);
+    } else {
+        id redoStack = [self.testUndoMgr valueForKey:@"_redoStack"];
+        NSLog(@"redo stack: %@", [redoStack valueForKey:@"_count"]);
+        id undoStack = [self.testUndoMgr valueForKey:@"_undoStack"];
+        NSLog(@"undo stack: %@", [undoStack valueForKey:@"_count"]);
+    }
 }
 
 @end
